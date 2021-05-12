2bcec9a6a93e5b05f4d1b78d684f04b79194ffeb
Chris Cann <canniman@googlemail.com>
2021-05-12 18:06:20 +0100

    set screen resolution on mobile for realz this time

--- Assets/_Project/Scripts/Game/Util/SetScreenResolution.cs
+++ Assets/_Project/Scripts/Game/Util/SetScreenResolution.cs
@@ -1,7 +1,18 @@
+using System;
+using UnityEngine;
+
 namespace Util
 {
-    public class SetScreenResolution
+    public class SetScreenResolution : MonoBehaviour
     {
-        
+        [SerializeField] private int width = 900;
+        [SerializeField] private int height = 1600;
+
+        private void Awake()
+        {
+#if UNITY_ANDROID
+                Screen.SetResolution(width, height, true);
+#endif
+        }
     }
 }

f3fedf9198b89f07baebe30809b38e6e3efe1883
chris-m-cann <christopher.m.cann@gmail.com>
2022-10-17 19:33:31 +0100

    fps counter for builds, test scene, attempt to optimise the render settings and the letterbox rendering

---
+++ Assets/_Project/Scripts/Game/Util/UI/FpsCounter.cs
@@ -0,0 +1,42 @@
+using System;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+
+namespace Util.UI
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class FpsCounter : MonoBehaviour
+    {
+        [SerializeField] private int averageOver;
+
+        private TMP_Text _text;
+
+        private float _time;
+        private int _count;
+
+        private void Awake()
+        {
+            _text = GetComponent<TMP_Text>();
+        }
+
+        private void Update()
+        {
+            _time += Time.deltaTime;
+            --_count;
+            if (_count < 0)
+            {
+                _text.text = CalculateFps();
+                _count = averageOver;
+                _time = 0f;
+            }
+        }
+
+        private string CalculateFps()
+        {
+            float fps = averageOver / _time;
+
+            return Mathf.RoundToInt(fps).ToString();
+        }
+    }
+}
